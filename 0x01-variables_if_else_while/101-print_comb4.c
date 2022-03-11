6fc1eaaf78d467aee14d26ecb3313efe0133802b
7malikk <7malikk@gmail.com>
2022-03-11 11:10:05 -0800

    last commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -21,24 +21,27 @@ int main(void)
 		j = i + 1;
 		while (j < 58)
 		{
-			putchar(i);
-			putchar(j);
-			putchar(k);
-			if (i < 55 || j < 56 || k < 57)
+			k = j +1;
+			while (k < 58)
 			{
-				putchar(44);
-				putchar(32);
+				putchar(i);
+				putchar(j);
+				putchar(k);
+				if (i < 55 || j < 56 || k < 57)
+				{
+					putchar(44);
+					putchar(32);
+				}
+				k++;
 			}
-			k++;
+
+			j++;
 		}
 
-		j++;
+		i++;
 	}
 
-	i++;
-}
+	putchar(10);
 
-putchar(10);
-
-return (0);
+	return (0);
 }
