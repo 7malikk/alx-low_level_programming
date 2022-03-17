de6fefe4384bfda0c12f0560b39908035d249bf4
Malik <75104021+7malikk@users.noreply.github.com>
2022-03-18 00:11:16 +0100

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers 0-9
- *
- * Return: void
- */
+* print_numbers - prints numbers 0-9
+*
+* Return: void
+*/
 
 void print_numbers(void)
 {
